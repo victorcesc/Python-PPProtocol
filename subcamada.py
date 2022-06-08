e0d6a11177bba6e3100a1930672fa98650a48a56
Jhonatan Constantino <jhonatan.c16@aluno.ifsc.edu.br>
2022-06-07 22:45:01 -0300

    feito envia e recebe da subcamada

--- subcamada.py
+++ subcamada.py
@@ -1,20 +1,19 @@
 from pypoller import poller
-
+from quadro import Quadro
 
 class Subcamada(poller.Callback):
-    
 
-    def __init__(self,*args):
-        poller.Callback.__init__(self,*args)
+    def __init__(self, *args):
+        poller.Callback.__init__(self, *args)
         self.upper = None
         self.lower = None
 
-    def envia(self,quadro):
-        raise NotImplementedError('abstrato')
+    def envia(self, quadro:Quadro):
+        self.lower.envia(quadro)
 
-    def recebe(self,quadro):
-        raise NotImplementedError('abstrato')
+    def recebe(self, quadro:Quadro):
+        self.lower.recebe(quadro)
 
-    def conecta(self,superior):
+    def conecta(self, superior):
         self.upper = superior
         superior.lower = self 
