87a2f7cdf36b05e47e79fbd2d9981869a8592fe7
Jhonatan Constantino <jhonatan.c16@aluno.ifsc.edu.br>
2022-06-14 17:22:14 -0300

    adicionado fcs ao quadro

--- quadro.py
+++ quadro.py
@@ -1,4 +1,5 @@
 
+import crc
 
 '''
 byte controle
@@ -11,7 +12,7 @@ bit 0 e 1  =  {00 = CR // 01 = CC // 10 = DR // 11 = DC}
 
 class Quadro:
 
-    def __init__(self, tipo, sequencia, reservado, proto, data,fcs):
+    def __init__(self, tipo, sequencia, reservado, proto, data):
         self.tipo = tipo
         self.sequencia = sequencia
         self.reservado = reservado
@@ -19,19 +20,36 @@ class Quadro:
         if self.tipo == 0:
             self.proto = proto    
         self.data = data
-        self.fcs = fcs
- 
 
     def serialize(self):
+        # cria um vetor de bytes
         self.quadro = bytearray()
-        self.quadro.append(0x7e)
-        self.controle |= (self.tipo << 7)
-        self.controle |= (self.sequencia << 3)
-        self.quadro += self.controle
-        self.quadro += self.reservado
-        self.quadro += self.proto
-        self.quadro += self.data
-        self.quadro += self.fcs
+        # adiciona o delimitador do quadro
         self.quadro.append(0x7e)
 
-   
+        # modifica o bit 7 do byte de controle para indicar o tipo
+        # e o bit 3 para indicar a sequencia
+        self.controle |= (7 << self.tipo)
+        self.controle |= (3 << self.sequencia)
+
+        # adiciona o byte de controle ao quadro
+        self.quadro.append(self.controle)
+        self.quadro.append(self.reservado)
+
+        # se o tipo é 0 (dados)
+        # adiciona o byte de proto ao quadro
+        if self.tipo == 0:
+            self.quadro.append(self.proto)
+
+        # adiciona os dados ao quadro
+        self.quadro += self.data.encode()
+
+        # gerar crc e formata o quadro com o fcs no final
+        fcs = crc.CRC16(self.quadro)
+        self.quadro = fcs.gen_crc()
+
+        # delimitador de quadro
+        self.quadro.append(0x7e)
+
+        # retorna o quadro
+        return self.quadro
