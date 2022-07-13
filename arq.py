673c237cd4c8aaf8ea0edcb75b47bb219da37ac4
victorcesc <victorcesconetto@bry.com.br>
2022-07-12 22:48:57 -0300

    resolvido o problema do descartar?

--- arq.py
+++ arq.py
@@ -15,22 +15,32 @@ class Arq(Subcamada):
         self._fsm = self.state_ocioso
         self.quadro = None
         self.debug = False
+        self.sequencia_M = 0
+        self.sequencia_N = 0
+        self.queue_msg = []
   
     def recebe(self, quadro:Quadro):
         self.quadro = quadro
+        self.sequencia_M = quadro.sequencia
+        #self.queue_msg.append(quadro)
         self._fsm(quadro)
         if self.debug:
                 print('[ARQ]: entregando quadro para camada superior, tamanho =', len(quadro.serialize()))
       # dados recebidos da subcamada inferior
+        
+
 
     def envia(self,quadro:Quadro):
+        print('[ARQ]enviando quadro sequencia : ', quadro.sequencia)
         self.quadro = quadro
+        self.sequencia_N = quadro.sequencia
         if quadro.data == "reset":
             self._fsm = self.state_ocioso
             self.quadro = None
         if self._fsm == self.state_ocioso:
            self.lower.envia(quadro) # !dataN
            self._fsm = self.state_espera
+        
         # self._fsm(quadro)
         
     # dataM = 0 recebendo
@@ -39,30 +49,47 @@ class Arq(Subcamada):
         # M = 0
         # _M = 1
         self.enable_timeout()        
-        if quadro.tipoMsgArq == 0 and quadro.sequencia == self.quadro.sequencia and  quadro.idSessao == self.quadro.idSessao:# dataM 
-            ack  = Quadro(tiposessao = 0,msgarq = 1,sequencia = self.quadro.sequencia,idsessao = quadro.idSessao)          
+        if quadro.tipoMsgArq == 0 and quadro.sequencia == self.sequencia_M and  quadro.idSessao == self.quadro.idSessao:# dataM 
+            print("[ARQ_ocioso1] Sequencia do quadro recebido", quadro.sequencia, "sequencia esperada", self.sequencia_M)
+            
+            ack  = Quadro(tiposessao = 0,msgarq = 1,sequencia = self.sequencia_M,idsessao = quadro.idSessao)          
             # ack idSessao tem q ser igual ao quadro data recebido
+            print("[ARQ] Enviando ack com sequencia : ", ack.sequencia)
             self.lower.envia(ack) # envia pra camada de baixo         
             self.upper.recebe(quadro) # envia p camada de cima
-        if (quadro.tipoMsgArq == 0 and quadro.sequencia !=  self.quadro.sequencia) and  quadro.idSessao == self.quadro.idSessao: # data_M
-            ack_M  = Quadro(tiposessao = 0,msgarq = 1,sequencia = self.quadro.sequencia,idsessao = quadro.idSessao)
+            self._fsm = self.state_ocioso
+
+        if quadro.tipoMsgArq == 0 and quadro.sequencia !=  self.sequencia_M and  quadro.idSessao == self.quadro.idSessao: # data_M
+            print("[ARQ_ocioso2] sequencia do quadro recebido", quadro.sequencia, "sequencia esperada", self.sequencia_M, "e foi descartado")
+            ack_M  = Quadro(tiposessao = 0,msgarq = 1,sequencia = not self.sequencia_M,idsessao = quadro.idSessao)
+            print("[ARQ] Enviando ack com sequencia", ack_M.sequencia)
             self.lower.envia(ack_M) # envia p camada de baixo
+            self._fsm = self.state_ocioso
+
             # self._fsm = self.state_espera
 
     def state_espera(self, quadro:Quadro):
-        if quadro.tipoMsgArq == 0 and quadro.sequencia == self.quadro.sequencia and  quadro.idSessao == self.quadro.idSessao: # ackM , app!msg
-            ack  = Quadro(tiposessao = 0,msgarq = 1,sequencia = self.quadro.sequencia,idsessao = quadro.idSessao)
+        if quadro.tipoMsgArq == 0 and quadro.sequencia == self.sequencia_M and  quadro.idSessao == self.quadro.idSessao: # ackM , app!msg
+            print("[ARQ_espera1] sequencia do quadro recebido", quadro.sequencia, "sequencia esperada", self.sequencia_M)
+            ack  = Quadro(tiposessao = 0,msgarq = 1,sequencia = self.sequencia_M,idsessao = quadro.idSessao)
+            print("[ARQ] Enviando ack com sequencia", ack.sequencia)
             self.lower.envia(ack)
             self.upper.recebe(quadro)
-            
-        if (quadro.tipoMsgArq == 0 and quadro.sequencia != self.quadro.sequencia) and  quadro.idSessao == self.quadro.idSessao  : # data_M
-            ack_M  = Quadro(tiposessao = 0,msgarq = 1,sequencia = not self.quadro.sequencia,idsessao = quadro.idSessao)
-            self.lower.envia(ack_M)
-            
-        if (quadro.tipoMsgArq == 1 and quadro.sequencia != self.quadro.sequencia) and  quadro.idSessao == self.quadro.idSessao: # ack_N
             self._fsm = self.state_espera
             
-        if quadro.tipoMsgArq == 1 and quadro.sequencia == self.quadro.sequencia and  quadro.idSessao == self.quadro.idSessao: # ackN
+        if quadro.tipoMsgArq == 0 and quadro.sequencia != self.sequencia_M and  quadro.idSessao == self.quadro.idSessao  : # data_M
+            print("[ARQ_espera2] sequencia do quadro recebido", quadro.sequencia, "sequencia esperada", not self.sequencia_M)
+            ack_M  = Quadro(tiposessao = 0,msgarq = 1,sequencia = not self.sequencia_M,idsessao = quadro.idSessao)
+            print("[ARQ] Enviando ack com sequencia", ack_M.sequencia)
+            self.lower.envia(ack_M)
+            self._fsm = self.state_espera
+            
+        if quadro.tipoMsgArq == 1 and quadro.sequencia != self.sequencia_N and  quadro.idSessao == self.quadro.idSessao: # ack_N
+            print("[ARQ_espera3] sequencia do quadro recebido", quadro.sequencia, "sequencia esperada", self.sequencia_N, "e mantem no estado espera")
+            self._fsm = self.state_espera
+            
+        if quadro.tipoMsgArq == 1 and quadro.sequencia == self.sequencia_N and  quadro.idSessao == self.quadro.idSessao:
+            print("[ARQ_espera4] sequencia do quadro recebido", quadro.sequencia, "sequencia esperada", self.sequencia_N,"tudo ok e retornou ao ocuioso") # ackN
             self._fsm = self.state_ocioso
 
     def handle(self):      
